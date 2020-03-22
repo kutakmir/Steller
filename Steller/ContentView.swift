2cf37e10492809bcb22bc46429d7c2e17bb96473
Miroslav Kutak <kutakmiroslav@gmail.com>
2020-03-22 19:49:28 +0100

    Initial Commit

---
+++ Steller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Steller
+//
+//  Created by Miroslav Kutak on 22/03/2020.
+//  Copyright © 2020 Miroslav Kutak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
